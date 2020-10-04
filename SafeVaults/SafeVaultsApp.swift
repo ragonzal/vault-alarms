880188ee8f815b2e66e17b2083773b2f2ee4cbcc
Ramiro Gonzalez <ramiro@xivis.com>
2020-10-04 12:47:13 -0300

    Initial Commit

---
+++ SafeVaults/SafeVaultsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeVaultsApp.swift
+//  SafeVaults
+//
+//  Created by Ramiro Gonzalez on 04/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct SafeVaultsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
